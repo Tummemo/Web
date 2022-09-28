bb7fcfe34dc96d708718f9befc2b36216e4b9168
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-09-28 12:41:47 +0630

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,12 @@ $db_user = "kjuiegkkkxmozq";
 $db_pwd = "bf37450b31c08217f94fadc0cba06338370dd9ae484303d1131b48438ed8126f";
 $db_name = "d6ribukhttlnbt";
 
-$conn = mysqli_connect($db_host,$db_user,$db_pwd,$db_name) or die("database connection error");
 
-
-?>
+try {
+  $conn = new PDO("mysql:host=$db_host;db_name="d6ribukhttlnbt", $db_user, $db_pwd);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
