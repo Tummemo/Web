ea9b8613c95b2eea33133582b6f12c74d0ff85db
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-09-28 12:05:52 +0630

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
+<?php
+
 $db_host = "ec2-54-91-223-99.compute-1.amazonaws.com";
 $db_user = "tsvxfgfiuyfkqm";
 $db_pwd = "aaa7f886bdb3ddb967f4536e6b36ef779c84d9255803947574e48d8a2563272a";
@@ -5,3 +7,4 @@ $db_name = "d2mvfsh6g23hv1";
 
 $conn = mysqli_connect($db_host,$db_user,$db_pwd,$db_name) or die("database connection error");
 
+?>
