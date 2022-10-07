d4bbd898669a3af325cd46c9c5995d3f54fb7e70
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-07 12:29:05 +0630

    Create pageHeadFoot.php

---
+++ LAYOUT/pageHeadFoot.php
@@ -0,0 +1,20 @@
+<?php
+include("JS/script.php");
+function pageHeader($title,$nav){
+
+?>
+
+<html>
+<head><title>$title</title>
+<link rel="stylesheet" href="CSS/style.css">
+</head><body>
+
+<nav> <h1>$nav</h1> </nav>
+<?php
+} function pageFooter(){ 
+?>
+</body>
+</html>
+<?php
+}
+?>
