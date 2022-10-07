aee6e6237a0ee583a0ea70d14136c7969add9831
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-07 12:30:22 +0630

    Create login.php

---
+++ LAYOUT/login.php
@@ -0,0 +1,16 @@
+<?php 
+function pageLogin() { ?>
+<center> 
+<form action="" method="POST">
+    <table>
+        <tr><td><label>Username</label></td><td><input type="text" name="user"></td></tr>
+        <tr><td><label>Password</label></td><td><input type="password" name="pwd"></td></tr> 
+        <tr><td><label>Login</label></td><td><input type="submit" name="submit" value="Login"></td></tr>
+    </table>
+    
+    
+    
+</form>
+</center>
+
+<?php } ?>
