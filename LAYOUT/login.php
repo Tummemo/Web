1977268ceb105cde1284c7dee5f42c114ac3e012
Tummemo <113905858+Tummemo@users.noreply.github.com>
2022-10-08 00:56:23 +0630

    Update login.php

--- LAYOUT/login.php
+++ LAYOUT/login.php
@@ -6,7 +6,16 @@ function pageLogin() { ?>
     <table>
         <tr><td><label>Username</label></td><td><input type="text" name="user"></td></tr>
         <tr><td><label>Password</label></td><td><input type="password" name="pwd"></td></tr> 
-        <tr><td><label>Login</label></td><td><input type="submit" onclick="hello();" name="submit" value="Login"></td></tr>
+        <tr><td><label>
+        <select name="level">
+        <option name="none">
+Level
+</option><option name="student">Student</option>
+</option name="teacher">Teacher</option>
+        </select>
+         </label></td>
+
+         <td><input type="submit" onclick="hello();" name="submit" value="Login"></td></tr>
     </table>
     
     
